9eb6d2dfb668220464c0a3fe8d570da8f77d4a31
Yutao <yuta_o@outlook.com>
2017-12-25 21:17:11 +0800

    12.25

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -1,29 +1,33 @@
 const electron = require('electron')
-    // Module to control application life.
+// Module to control application life.
 const app = electron.app
-    // Module to create native browser window.
+// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
+const Tray = electron.Tray
+const Menu = electron.Menu
 const path = require('path')
 const url = require('url')
-
+const mainmenu = require('./mainmenu.js')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let tray
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600, frame: true });
-
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: true
+    });
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
-
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
@@ -32,13 +36,15 @@ function createWindow() {
         // when you should delete the corresponding element.
         mainWindow = null;
     });
-}
 
+    Menu.setApplicationMenu(mainmenu())
+
+    tray = new Tray("icon.png")
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
@@ -47,7 +53,6 @@ app.on('window-all-closed', function() {
         app.quit();
     }
 });
-
 app.on('activate', function() {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -55,6 +60,5 @@ app.on('activate', function() {
         createWindow();
     }
 });
-
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
