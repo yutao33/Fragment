546d26d46efd57b9d09df98356d2cbef5402c879
Yutao <yuta_o@outlook.com>
2018-01-02 22:54:55 +0800

    1.2

--- courses/MachineLearning/ica/audio.py
+++ courses/MachineLearning/ica/audio.py
@@ -2,9 +2,10 @@ import struct
 import numpy as np
 from pydub import AudioSegment
 import matplotlib.pyplot as plt
-import ctypes
+from sklearn.decomposition import FastICA
+from mdp import fastica
 
-def readaudio(path,format='mp3'):
+def readaudio1(path,format='mp3'):
     """
     read an audio
     return numpy.array,frame_rate
@@ -31,18 +32,83 @@ def readaudio(path,format='mp3'):
             data[r,c]=k[0]/amax
     return data,sound.frame_rate
 
+def readaudio(path,format='mp3'):
+    """
+    read an audio
+    return numpy.array,frame_rate
+    """
+    sound = AudioSegment.from_file(path,format=format)
+    raw=sound.raw_data
+    w=sound.sample_width
+    if w==1:
+        dtype=np.int8
+    elif w==2:
+        dtype=np.int16
+    elif w==4:
+        dtype=np.int32
+    else:
+        raise Exception("unexcepted sample_width")
+    amax=2**(w*8-1)
+    col=sound.channels
+    row=len(raw)//w//col
+    data=np.ndarray([row,col],dtype=dtype,buffer=raw)
+    data=data/amax
+    return data,sound.frame_rate
 
 def writeaudio(path,data,frame_rate=44100,sample_width=2,format='mp3'):
     """
     write an audio
 
     """
-    sound = AudioSegment()
+    w=sample_width
+    if w==1:
+        dtype=np.int8
+    elif w==2:
+        dtype=np.int16
+    elif w==4:
+        dtype=np.int32
+    else:
+        raise Exception("unexcepted sample_width")
+    amax=2**(w*8-1)
+    d=dtype(data*amax).tobytes()
+    sound = AudioSegment(data=d,sample_width=sample_width,frame_rate=frame_rate,channels=data.shape[1])
+    sound.export(out_f=path,format=format)
 
 def test1():
     data,fr=readaudio(r"C:\Users\yutao\Downloads\lz.mp3")
-    plt.plot(data[44444:55555])
-    plt.show()
+    data=data[4444:len(data)//2]
+    writeaudio(r"C:\Users\yutao\Downloads\out.mp3",data)
+
+def mix():
+    data1,fs1=readaudio(r"C:\Users\yutao\Downloads\lz.mp3")
+    data2,fs2=readaudio(r"C:\Users\yutao\Downloads\mhh.mp3")
+    assert fs1==fs2
+    data1=data1[:,0]
+    data2=data2[:,0]
+    l=min(len(data1),len(data2))-1
+    mixed1=0.4*data1[0:l]+0.4*data2[0:l]
+    mixed2=0.4*data1[0:l]+0.3*data2[0:l]
+    #mixed1=data1[0:l]
+    #mixed2=data2[0:l]
+    print(mixed1.shape)
+    print(mixed2.shape)
+    d=np.c_[mixed1,mixed2]
+    writeaudio(r"C:\Users\yutao\Downloads\out.wav",d,format='wav')
+
+
+def unmix():
+    data,fs=readaudio(r"C:\Users\yutao\Downloads\out.wav")
+    # d=FastICA().fit(data).transform(data)
+    print(data.shape)
+    d=fastica(data)
+    d /= np.max(abs(d), axis = 0)
+    writeaudio(r"C:\Users\yutao\Downloads\out1.wav",d,format='wav')
+
+def unmix_1():
+    data,fs=readaudio(r"C:\Users\yutao\Downloads\mhh.mp3")
+    d=fastica(data)
+    d /= np.max(abs(d), axis = 0)
+    writeaudio(r"C:\Users\yutao\Downloads\mhh_unmix.wav",d,format='wav')
 
 if __name__=="__main__":
-    test1()
+    unmix_1()
