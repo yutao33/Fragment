fa1e23241af03549ce6509b5899727b0c5624c9a
Yutao <yuta_o@outlook.com>
2018-01-30 16:03:27 +0800

    yutao-Plex

--- mininet/topologyzoo-generator/topogen.py
+++ mininet/topologyzoo-generator/topogen.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 
+import os
+import math
 import networkx as nx  # 1.10
 import matplotlib.pyplot as plt
 
@@ -50,11 +52,11 @@ class GLink(object):
         return "GLink[%s,%s]"%(str(self.node1),str(self.node2))
 
 
-def startnet(switchlist,hostlist,linklist):
+def confignet(switchlist,hostlist,linklist):
     setLogLevel('info')
     net = Mininet(topo=None,
                   build=False,
-                  ipBase='10.0.0.0/8')
+                  ipBase='10.0.0.0/8',autoSetMacs=True)
 
     info('*** Adding controller\n')
     c0 = net.addController(name='c0',
@@ -78,6 +80,11 @@ def startnet(switchlist,hostlist,linklist):
         assert isinstance(l,GLink)
         net.addLink(l.node1.n,l.node2.n)
 
+    for s in switchlist:
+        del s.n
+    for h in hostlist:
+        del h.n
+
     info('*** Starting network\n')
     net.build()
     info('*** Starting controllers\n')
@@ -89,12 +96,17 @@ def startnet(switchlist,hostlist,linklist):
         assert isinstance(s,GSwitch)
         net.get(s.name).start([c0])
     info('*** Post configure switches and hosts\n')
+    return net
+
+def startnet(switchlist,hostlist,linklist):
+    net = confignet(switchlist, hostlist, linklist)
+
     CLI(net)
     net.stop()
 
 
 if __name__ == "__main__":
-    topology_name = "Noel"  # Noel Carnet Sprint
+    topology_name = "Noel"  # Noel Carnet Sprint Geant2012
     filename = "/home/yutao/Work/topologyzoo/topologyzoo-graphml2/sources/" + \
                topology_name + ".graphml"
     topo = nx.read_graphml(filename).to_undirected()
@@ -103,9 +115,12 @@ if __name__ == "__main__":
         labels[n] = n
     # pos = nx.spring_layout(topo)
     pos = nx.fruchterman_reingold_layout(topo, center=(0, 0))
-    nx.draw(topo,pos=pos)
-    nx.draw_networkx_labels(topo,pos,labels=labels)
-    plt.show()
+
+    draw = False
+    if draw:
+        nx.draw(topo,pos=pos)
+        nx.draw_networkx_labels(topo,pos,labels=labels)
+        plt.savefig('topo.png')
 
     s = {str(n): GSwitch(str(n)) for n in topo.adj}
     l = {}
@@ -119,11 +134,34 @@ if __name__ == "__main__":
     hostlist = []
     switchlist = s.values()
     linklist = l.values()
-    tmp = sorted(pos.items(),key=lambda t:t[1][0]**2+t[1][1]**2,reverse=True)
-    for i in range(len(tmp)*2/3):
-        n=tmp[i][0]
-        host=GHost("h"+str(i),ip="10.0.0.%d"%(i+1))
+
+    # tmp = sorted(pos.items(),key=lambda t:t[1][0]**2+t[1][1]**2,reverse=True)
+    # for i in range(len(tmp)*2/3):
+    #     n=tmp[i][0]
+    #     host=GHost("h"+str(i),ip="10.0.0.%d"%(i+1))
+    #     hostlist.append(host)
+    #     linklist.append(GLink(host,s[n]))
+
+    tmp = {(n, math.sqrt(p[0] ** 2 + p[1] ** 2)) for n, p in pos.items()}
+    tmp = sorted(tmp,key=lambda t:t[1],reverse=True)
+    delta=tmp[0][1]*2/3
+    for i,a in enumerate(tmp,1):
+        n=a[0]
+        host=GHost("h"+str(i),ip="10.0.0.%d"%(i))
         hostlist.append(host)
         linklist.append(GLink(host,s[n]))
+        id='hs'+str(i)
+        topo.adj[host.name]={s[n].name:{'id':id}}
+        topo.adj[s[n].name][host.name]={'id':id}
+        topo.node[host.name]={'y':'0','x':'0'}
+        if a[1]<delta:
+            break
+    # topo.edge=topo.adj
+    # if draw:
+    #     pos = nx.fruchterman_reingold_layout(topo, center=(0, 0))
+    #     nx.draw(topo,pos=pos)
+    #     nx.draw_networkx_labels(topo,pos,labels=labels)
+    #     plt.savefig('topo1.png')
+    #     exit(0)
 
-    # startnet(switchlist,hostlist,linklist)
+    startnet(switchlist,hostlist,linklist)
