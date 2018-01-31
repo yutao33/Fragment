c055e2bd2d335bc795785aaa805659f0cebdd20e
Yutao <yuta_o@outlook.com>
2018-02-01 03:06:00 +0800

    acfun

--- mininet/README.md
+++ mininet/README.md
@@ -1,7 +1,5 @@
 
-# test3
-
-### 测试环境:
+# 测试环境:
 
 * Intel(R) Core(TM) i7-6700 CPU @ 3.40GHz
 * 8G DDR4 memory + 4G swap
@@ -9,6 +7,55 @@
 * OpenDaylight Carbon
 * ONOS 1.12.0
 * Mininet 2.3.0d1
+
+
+# test1
+
+获取Mininet启动之后第一次执行pingall命令的消耗时间，衡量控制器的响应速度
+
+### 结果:
+
+![result1_1](./plot/figs/test1_Geant2012_40_101.png)
+
+其中
+
+* SP : 使用最短路径的应用,生成的流表会匹配端口，源和目的地址
+* ST : 以目的端口为根节点，计算最小生成树，生成的流表匹配端口和目的地址
+* Flood : 以一个固定端口为根，且计算过程中保持一定的顺序，对整个拓扑计算一个固定的最小生成树，生成的流表只匹配端口
+* ONOS : ONOS-fwd应用
+* ODL : ODL l2switch应用
+
+可以得到的结论有，
+
+* 可以用不同的算法实现L2Switch的应用, 效率上也不相同
+* 和ONOS相比，响应速度都快于ONOS
+* 和ODL相比，因为ODL-l2switch网络内部链路使用广播方式，和Flood应用相比，Flood应用的响应速度也更快
+
+# test2
+
+使用一个周期的udp数据流测量网络的恢复时间
+
+
+### 结果:
+
+switch down:
+
+![result](./plot/figs/test2_1.png)
+
+link down:
+
+![result](./plot/figs/test2_2.png)
+
+测试时，在link down的情况下ONOS无法恢复
+
+可以得到的结论有，
+
+* 系统可以快速恢复
+
+
+# test3
+
+
 
 ### 测试拓扑: Noel
 
@@ -24,7 +71,8 @@ Server节点运行nginx 1.10.3
 
 Client节点使用apache benchmark (2.3)测试传输速率
 
-测试命令类似`ab -c 10 -n 100 http://10.0.0.7/50M.dat`, `-c 10` 表示并发量为10，nginx不会成为bottleneck，测试P3分别使用了1M,10M,50M的数据量，其他测试结果为50M的数据量，因为其他测试结果不会受到数据量影响，即时测试也是很接近的数值
+测试命令类似`ab -c 10 -n 100 http://10.0.0.7/50M.dat`, `-c 10` 表示并发量为10，nginx不会成为bottleneck，测试P3分别使用了1M,10M,50M的数据量，其他测试结果为50M的数据量，因为其他测试结果不会受到数据量影响，测试结果数值接近
+
 ### 结果:
 
 ![result](./plot/figs/test3.png)
--- mininet/plot/eval.py
+++ mininet/plot/eval.py
@@ -7,16 +7,64 @@ matplotlib.rcParams['figure.figsize']=[6.4*0.8,4.8*0.8]
 matplotlib.rcParams['font.size']=12
 
 themecolor1 = 'skyblue'
-themecolor2 = '#FFFC7F'
+themecolor2 = '#EEEE00'
+themecolor3 = '#FF9933'
 
-def plotgraph(name,data,tick_label,ylabel,color):
+
+
+systemname = 'K${}_3$ATAU'
+
+def plotgraph(name,data,tick_label,ylabel,color,xtick_rotation=0):
     plt.clf()
-    plt.bar(range(len(data)), data,width=min(0.8,0.15*len(data)),tick_label=tick_label,color=color)
+    x =range(len(data))
+    plt.bar(x, data,width=min(0.8,0.15*len(data)),color=color)
+    ax = plt.axes()
+    ax.set_xticks(x)
+    ax.set_xticklabels(tick_label,rotation=xtick_rotation,fontsize='small')
     for i in range(len(data)):
         y = data[i]
         plt.text(i,y,'%.2f'%y,ha='center',va='bottom')
     plt.ylabel(ylabel)
     plt.savefig('figs/'+name+'.png',bbox_inches='tight')
+
+
+
+data = [[0.695, 0.502, 0.253,   0.945, 0.144],
+        [17.202,5.916, 1.72, 71.578,3.234],
+        [0.234,0.185,  0.12,  0.376,0.130],
+        [2.495,1.686,  0.387, 4.23,0.685]]
+la =[systemname+'-SP',systemname+'-ST',systemname+'-Flood','ONOS-fwd','ODL-L2Switch']
+
+color = [themecolor1,themecolor1,themecolor1,themecolor3,themecolor3]
+
+topo=['Sprint_11_29','Geant2012_40_101','Basnet_7_13','Noel_19_44']
+
+for i,d in enumerate(data):
+    plotgraph('test1_%s'%topo[i],d,la,'pingall time(s)',color,xtick_rotation=15)
+
+
+
+data = [[45.25, 65.25, 2059.75],
+        [33.75, 0, 4004.25]]
+la =[systemname,'ONOS','ODL']
+color = [themecolor1,themecolor2,themecolor3]
+
+for i,d in enumerate(data,1):
+    plotgraph('test2_%d'%i,d,la,'recovery time(ms)',color)
+
+
+
+
+matplotlib.rcParams['figure.figsize']=[6.4,4.8]
+matplotlib.rcParams['font.size']=10
+
+data = [116.305, 23.343, 27.729,      104.579,       115.722,      115.594, 31.103]
+la =   ['P1'   , 'P2'  , 'P3-1', 'P3-2', 'P3-3',  'ONOS',  'ODL']
+color = [*(themecolor1,)*2, *(themecolor2,)*3, *(themecolor3,)*2]
+plotgraph('test3',data,la,'traffic rate(Mbytes/s)',color)
+
+
+
 
 
 
@@ -50,45 +98,5 @@ plotgraph('figure3_2',data,la,'traffic rate(Mbytes/sec)',color)
 
 '''
 
-#########################################################
-
-# data = [116.305,23.343,115.722,115.594,31.103]
-# la = ['P1','P2','P3','ONOS','ODL']
-# color = themecolor1
-# plotgraph('test3',data,la,'traffic rate(Mbytes/s)',color)
-
-data = [[0.695, 0.502, 0.253,   0.945, 0.144],
-        [17.202,5.916, 1.72, 71.578,3.234],
-        [0.234,0.185,  0.12,  0.376,0.130],
-        [2.495,1.686,  0.387, 4.23,0.685]]
-la =['SP','Spanning','Flood','ONOS','ODL']
-
-color = [themecolor1,themecolor1,themecolor1,themecolor2,themecolor2]
-
-topo=['Sprint_11_29','Geant2012_40_101','Basnet_7_13','Noel_19_44']
-
-for i,d in enumerate(data):
-    plotgraph('test1_%s'%topo[i],d,la,'pingall time(s)',color)
-
-
-
-data = [[45.25, 2059.75, 65.25],
-        [33.75, 4004.25, 0]]
-la =['SP','ODL','ONOS']
-color = [themecolor1,themecolor2,themecolor2]
-
-for i,d in enumerate(data,1):
-    plotgraph('test2_%d'%i,d,la,'recovery time(ms)',color)
-
-
-
-
-matplotlib.rcParams['figure.figsize']=[6.4,4.8]
-matplotlib.rcParams['font.size']=10
-
-data = [116.305, 23.343, 27.729,      104.579,       115.722,      115.594, 31.103]
-la =   ['P1'   , 'P2'  , 'P3-1', 'P3-2', 'P3-3',  'ONOS',  'ODL']
-color = [*(themecolor1,)*5,*(themecolor2,)*2]
-plotgraph('test3',data,la,'traffic rate(Mbytes/s)',color)
 
 
