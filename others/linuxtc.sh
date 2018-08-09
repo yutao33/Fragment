d707f44e8c95d5ee0d69e4aef454269d54fd3f65
Yutao <yuta_o@outlook.com>
2018-08-09 20:51:57 +0800

    cleanup

--- others/linuxtc.sh
+++ others/linuxtc.sh
@@ -4,7 +4,7 @@
 #Setup the rate control and delay
 sudo tc qdisc add dev lo root handle 1: htb default 12 
 sudo tc class add dev lo parent 1:1 classid 1:12 htb rate 56kbps ceil 128kbps 
-sudo tc qdisc add dev lo parent 1:12 netem delay 200ms
+sudo tc qdisc add dev lo parent 1:12 netem delay 50ms
 
 #Remove the rate control/delay
 sudo tc qdisc del dev lo root
